7f99417157d4f913d1e157c02c34c4d14adafd0f
TanujTanmaya <shubhamstudy45@gmail.com>
2025-11-22 01:16:09 +0530

    Fixing the linting issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-   let tanuj = 'unused variable here';
+  //  let tanuj = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
